ee3650c3aec16da92a9d90c61a2c18ae7eb9f1bd
TEAMCHINA <ckchinn8@hotmail.com>
2013-11-22 20:31:22 -0800

    Adding tests and initial web client driver code.

---
+++ Selenite.Tests/Drivers/WebClientDriverTests.cs
@@ -0,0 +1,20 @@
+﻿using Selenite.Services.Implementation;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Selenite.Tests.Drivers
+{
+    public class WebClientDriverTests
+    {
+        [Theory]
+        [InlineData("http://github.com", "<title>GitHub")]
+        public void NavigateTo(string url, string expectedText)
+        {
+            var webClientDriver = new WebClientDriver();
+            webClientDriver.Navigate().GoToUrl(url);
+
+            Assert.Equal(url, webClientDriver.Url);
+            Assert.Contains(expectedText, webClientDriver.PageSource);
+        }
+    }
+}
